33d7b076661a9ab73de1326959a01a5182092aa0
samyakrc <samyakrc@gmail.com>
2017-10-30 11:50:11 -0700

    readyState

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
+import {DishService} from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +23,7 @@ import { DishDetailComponent } from './dish-detail/dish-detail.component';
     MaterialModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
