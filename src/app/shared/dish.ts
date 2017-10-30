5ce714f85bec50e43f81f5163db6c2e87d9491a4
samyakrc <samyakrc@gmail.com>
2017-10-30 11:04:32 -0700

    readyState

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import {Comment} from './comment';
+
 export class Dish 
 {
     name: string;
@@ -6,4 +8,5 @@ export class Dish
     label: string;
     price: string;
     description: string;
+    comments: Comment[];
 }
